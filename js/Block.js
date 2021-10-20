ebc792d2044ec6c8043ce7defc3362dc39a05a1a
sharanyamehta08 <83710551+sharanyamehta08@users.noreply.github.com>
2021-10-20 17:42:20 +0530

    Add files via upload

---
+++ js/Block.js
@@ -0,0 +1,32 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+            restitution :0.4,
+            friction :0.0,
+            // set the isStatic property 
+            isStatic : false
+        }
+        
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.visibilty = 255; 
+        World.add(world, this.body);
+      }
+      display(){
+        if (this.body.speed < 3){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+        }
+        else {
+          World.remove(world,this.body)
+        }
+
+      }
+}
